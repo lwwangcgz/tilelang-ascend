da443dad26d9aa47be62e8d170abd3fe7314498e
handsomeRobotSK <2034073486@qq.com>
2026-01-16 20:19:27 +0800

    [Fix]

--- src/target/codegen_ascend_pto.cc
+++ src/target/codegen_ascend_pto.cc
@@ -1161,10 +1161,8 @@ void CodeGenTileLangAscendPto::ReduceOpCodegen(const CallNode *op) {
 
   if (param2 != row_src || param3 != col_src) {
     this->PrintIndent();
-    this->stream << kAscendPtoScope << "slice_buffer_with_valid <" << ub_data_type << ", " << row_src << ", " << col_src << ", " << param2 << ", " << param3 << "> ();\n";
+    this->stream << kAscendPtoScope << "slice_buffer_with_valid <" << ub_data_type << ", " << row_src << ", " << col_src << ", " << param2 << ", " << param3 << "> (" << ffts <<");\n";
     var_names[1] = "tileUbWithValid";
-    this->PrintIndent();
-    this->stream << "TASSIGN(tileUbWithValid, " << ffts << ");";
   }
   
   // Determine whether to request the TileUbData with DN arrangement.
--- src/tl_templates/pto/common.h
+++ src/tl_templates/pto/common.h
@@ -191,7 +191,8 @@ AICORE PTO_INLINE void copy_ub_to_gm(
 }
 
 template <typename T1, int32_t rows, int32_t cols, int32_t validRow, int32_t validCol>
-AICORE PTO_INLINE void slice_buffer_with_valid() {
+AICORE PTO_INLINE void slice_buffer_with_valid(__ubuf__ *handle) {
     TileUbDataND<T1, rows, cols, validRow, validCol> tileUbWithValid;
+    TASSIGN(tileUbWithValid, handle);
 }
 }
