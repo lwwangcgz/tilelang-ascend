b7dc2390877a06d941d076f25f660d7158b432a4
handsomeRobotSK <2034073486@qq.com>
2026-01-17 14:18:13 +0800

    [Fix]

--- src/target/codegen_ascend_pto.cc
+++ src/target/codegen_ascend_pto.cc
@@ -1764,7 +1764,7 @@ void CodeGenTileLangAscendPto::AddFunction(const GlobalVar &gvar,
   use_swizzle_ = f->GetAttr<Bool>("use_swizzle").value_or(Bool(false));
   // tiling_map_ = f->GetAttr<Map<Var, PrimExpr>>("tiling_map").value_or(Map<Var, PrimExpr>());
   buffer_shapess_ = f->GetAttr<Map<Var, Array<PrimExpr>>>("buffer_shapess").value_or(Map<Var, Array<PrimExpr>>());
-  std::cout << "buffer_shapess = " << buffer_shapess << std::endl;
+  std::cout << "buffer_shapess = " << buffer_shapess_ << std::endl;
   var_sequence_ = f->GetAttr<Array<Var>>("var_sequence").value_or(Array<Var>());
   ICHECK(global_symbol.defined())
       << "CodeGenC: Expect PrimFunc to have the global_symbol attribute";
