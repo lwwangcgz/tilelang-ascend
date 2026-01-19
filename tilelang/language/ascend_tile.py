43824607ca2ebd18149fb277276e074630d76177
handsomeRobotSK <2034073486@qq.com>
2026-01-19 14:48:30 +0800

    [Fix]

--- tilelang/language/ascend_tile.py
+++ tilelang/language/ascend_tile.py
@@ -1341,7 +1341,7 @@ def clampMin(dst: Buffer, src: Buffer, tmp: Buffer, scalar_value: PrimExpr, coun
 
 def round(dst: Buffer, src: Buffer, tmp: Buffer, count: PrimExpr):
 
-    return tir.call_intrin(dst, src, "CAST_ROUND", count)
+    return cast(dst, src, "CAST_ROUND", count)
 
 def pow(dst: Buffer, src0: Buffer, src1: Buffer, tmp: Buffer):
     """Performs element-wise power calculation: dst = src0 ^ src1.
